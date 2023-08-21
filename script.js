85c1d28ff467bc39a2a6f51a067b925a27bd62a6
Pedro Ricciardi <riccip@outlook.com>
2023-08-21 10:58:23 +0100

    switch between light and night theme done

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-var theme;
+const toggle = document.getElementById("changeTheme");
 
+const storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light");
 
-theme = 'Dark';
-document.querySelectorAll('.main').forEach((selectedElement) => {
-  selectedElement.style.color = '#cccccc';
-  selectedElement.style.backgroundColor = '#333333';
+if (storedTheme) {
+  document.documentElement.setAttribute("data-theme", storedTheme);
+  if (storedTheme === "dark") {
+    document.getElementById("changeTheme").className = "changeTheme-dark";
+  } else {
+    document.getElementById("changeTheme").className = "changeTheme-light";
+  }
+}
+if (storedTheme)
+  document.documentElement.setAttribute("data-theme", storedTheme);
 
-});
-
-
-document.getElementById('changeTheme').addEventListener('click', (event) => {
-  if (theme == 'Dark') {
-    document.querySelectorAll('.main').forEach((selectedElement2) => {
-      selectedElement2.style.color = '#333333';
-      selectedElement2.style.backgroundColor = '#cccccc';
-
-    });
-    theme = 'Light';
-  } else if (theme == 'Light') {
-    document.querySelectorAll('.main').forEach((selectedElement3) => {
-      selectedElement3.style.color = '#cccccc';
-      selectedElement3.style.backgroundColor = '#333333';
-
-    });
-    theme = 'Dark';
+toggle.onclick = function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = currentTheme === "dark" ? "light" : "dark";
+  
+  if (targetTheme === "dark") {
+    document.getElementById("changeTheme").className = "changeTheme-dark";
+  } else {
+    document.getElementById("changeTheme").className = "changeTheme-light";
   }
 
-});
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+};
